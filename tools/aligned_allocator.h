7dc607f6589434f743795f6119ba687b67ed2481
James Wilson <jmw@fastmail.com>
2022-07-04 23:19:15 -0700

    Update aligned_allocator to use std::aligned_alloc

--- tools/aligned_allocator.h
+++ tools/aligned_allocator.h
@@ -20,89 +20,30 @@
 
 
 // The following headers are required for all allocators.
-#include <stddef.h>  // Required for size_t and ptrdiff_t and NULL
+#include <cstdlib>
+#include <cstddef>
+#include <limits>
 #include <new>       // Required for placement new and std::bad_alloc
 #include <stdexcept> // Required for std::length_error
 
-
-#ifdef WIN32
-#define __MSVCRT_VERSION__ 0x0700
-#include <malloc.h>
-#define MEMALIGN( array, alignment, size ) !(*array = _aligned_malloc( size, alignment ))
-#define FREE( array ) _aligned_free( array )
-#else
-#define MEMALIGN( array, alignment, size ) posix_memalign( array, alignment, size )
-#define FREE( array ) free( array )
-#endif
-
-
-template <typename T> class aligned_allocator
+template <typename T, std::align_val_t N> struct aligned_allocator
 {
-public:
 	// The following will be the same for virtually all allocators.
-	typedef T * pointer;
-	typedef const T * const_pointer;
-	typedef T& reference;
-	typedef const T& const_reference;
-	typedef T value_type;
-	typedef size_t size_type;
-	typedef ptrdiff_t difference_type;
-
-	T * address(T& r) const
-	{
-		return &r;
-	}
-
-	const T * address(const T& s) const
-	{
-		return &s;
-	}
-
-	size_t max_size() const
-	{
-		// The following has been carefully written to be independent of
-		// the definition of size_t and to avoid signed/unsigned warnings.
-		return (static_cast<size_t>(0) - static_cast<size_t>(1)) / sizeof(T);
-	}
-
-	// The following must be the same for all allocators.
-	template <typename U> struct rebind
-	{
-		typedef aligned_allocator<U> other;
-	};
-
-	bool operator!=(const aligned_allocator& other) const
-	{
-		return !(*this == other);
-	}
-
-	void construct(T * const p, const T& t) const
-	{
-		void * const pv = static_cast<void *>(p);
-		new (pv) T(t);
-	}
-
-	void destroy(T * const p) const; // Defined below.
-
-	// Returns true if and only if storage allocated from *this
-	// can be deallocated from other, and vice versa.
-	// Always returns true for stateless allocators.
-	bool operator==(const aligned_allocator& other) const
-	{
-		return true;
-	}
+	using pointer = T*;
+	using const_pointer = const T*;
+	using const_reference = const T&;
+	using value_type = T;
+	using size_type = std::size_t;
+	using difference_type = std::ptrdiff_t;
 
 	// Default constructor, copy constructor, rebinding constructor, and destructor.
 	// Empty for stateless allocators.
-	aligned_allocator() { }
-	aligned_allocator(const aligned_allocator&) { }
-	template <typename U> aligned_allocator(const aligned_allocator<U>&) { }
-	~aligned_allocator() { }
+	aligned_allocator() = default;
+	template <typename U> constexpr aligned_allocator(const aligned_allocator<U, N>&) noexcept {}
 
 	// The following will be different for each allocator.
-	T * allocate(const size_t n) const
+	[[nodiscard]] T* allocate(const std::size_t n) const
 	{
-//		std::cout << "Allocating " << n << (n == 1 ? " object" : "objects")	<< " of size " << sizeof(T) << "." << std::endl;
 		// The return value of allocate(0) is unspecified.
 		// aligned_allocator returns NULL in order to avoid depending
 		// on malloc(0)'s implementation-defined behavior
@@ -117,57 +58,23 @@ public:
 		// All allocators should contain an integer overflow check.
 		// The Standardization Committee recommends that std::length_error
 		// be thrown in the case of integer overflow.
-		if (n > max_size())
-		{
-			throw std::length_error("aligned_allocator<T>::allocate() - Integer overflow.");
+		if (n > std::numeric_limits<std::size_t>::max() / sizeof(T)) {
+			throw std::bad_array_new_length {};
 		}
 
 		// Allocators should throw std::bad_alloc in the case of memory allocation failure.
-		void * pv;
-		if (MEMALIGN( &pv, 16, n * sizeof(T)))
-			throw std::bad_alloc();
+		if (auto p = static_cast<T*>(std::aligned_alloc(static_cast<std::size_t>(N), n*sizeof(T)))) {
+			return p;
+		}
 
-		return static_cast<T *>(pv);
+		throw std::bad_alloc {};
 	}
 
-	void deallocate(T * const p, const size_t n) const
+	void deallocate(T* const p, [[maybe_unused]] const size_t n) const
 	{
-//		std::cout << "Deallocating " << n << (n == 1 ? " object" : "objects") << " of size " << sizeof(T) << "." << std::endl;
-		// aligned_allocator wraps free().
-		UNUSED(n);
-		FREE(p);
+		std::free(p);
 	}
-
-	// The following will be the same for all allocators that ignore hints.
-	template <typename U> T * allocate(const size_t n, const U * /* const hint */) const
-	{
-		return allocate(n);
-	}
-
-	// Allocators are not required to be assignable, so
-	// all allocators should have a private unimplemented
-	// assignment operator. Note that this will trigger the
-	// off-by-default (enabled under /Wall) warning C4626
-	// "assignment operator could not be generated because a
-	// base class assignment operator is inaccessible" within
-	// the STL headers, but that warning is useless.
-
-private:
-	aligned_allocator& operator=(const aligned_allocator&);
 };
 
-// A compiler bug causes it to believe that p->~T() doesn't reference p.
-#ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning(disable: 4100) // unreferenced formal parameter
-#endif
-
-// The definition of destroy() must be the same for all allocators.
-template <typename T> void aligned_allocator<T>::destroy(T * const p) const
-{
-	p->~T();
-}
-
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
+template <class T, class U, std::align_val_t NT, std::align_val_t NU>
+constexpr bool operator==(const aligned_allocator<T, NT>&, const aligned_allocator<U, NU>&) { return true; }
